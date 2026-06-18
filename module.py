c8dfb2ecf23658aadcc3ac02dd9cad7f6210d820
sc990389-sketch <sc990389@gmail.com>
2026-06-18 19:46:40 +0530

    done

---
+++ module.py
@@ -0,0 +1,17 @@
+import pyjokes
+
+
+# print("printing jokes...")
+
+
+# this print a random joke
+joke = pyjokes.get_joke()
+print(joke)
+
+
+
+
+# so thanks that was my program for printing jokes, I hope you liked it.     
+
+
+# another line of code
---
+++ problem 1.py
@@ -0,0 +1,61 @@
+# # in this folder you can find all the practice codes for python.
+
+# print('''Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+
+# When the blazing sun is gone,
+# When he nothing shines upon,
+# Then you show your little light,
+# Twinkle, twinkle, all the night.
+
+# Then the trav'ller in the dark,
+# Thanks you for your tiny spark,
+# He could not see which way to go,
+# If you did not twinkle so.
+
+# In the dark blue sky you keep,
+# And often thro' my curtains peep,
+# For you never shut your eye,
+# Till the sun is in the sky.
+
+# 'Tis your bright and tiny spark,
+# Lights the trav'ller in the dark:
+# Tho' I know not what you are,
+# Twinkle, twinkle, little star.'''
+# )
+
+
+# # question number 2
+
+# solved the sum question in terminal
+
+
+# # question number 3
+
+
+# import pyttsx3
+# engine = pyttsx3.init()
+
+# For Mac, If you face error related to "pyobjc" when running the `init()` method :
+# Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
+
+# engine.say("hi future devops engineer alok i know you r living in ireland and enjoying the life")
+# engine.runAndWait()
+
+
+
+# # question number 4
+
+# import os
+
+# path = input('/')
+
+# contents = os.listdir(path)
+
+# print("Contents of the directory:")
+
+# for item in contents:
+#     print(item)
+
